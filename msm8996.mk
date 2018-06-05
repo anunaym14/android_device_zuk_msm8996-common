7cbf390302d6101f0525d41f3b65150c4d91dc70
davidevinavil <davidevinavil@gmail.com>
2018-06-05 22:58:05 +0200

    msm8996-common: Move to Gcam

--- msm8996.mk
+++ msm8996.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
     camera.msm8996 \
     libmm-qcamera \
     mm-qcamera-app \
-    Snap \
+    Gcam \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
